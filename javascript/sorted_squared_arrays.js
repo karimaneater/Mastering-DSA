47cb9280a6704603eaab5bd136542d264e41745f
Rollen Ricafort <rollenricafort011@gmail.com>
2024-08-27 06:37:08 +0800

    sorting-array-WIP

--- javascript/sorted_squared_arrays.js
+++ javascript/sorted_squared_arrays.js
@@ -1,8 +1,38 @@
 var arr = [];
+var val = document.getElementById("validation");
+var sortedArr = [];
 
-function sortedSquaredArrays(){
+function getArray(){
    var num = document.getElementById("inputField").value;
-   arr.push(num);
-   console.log(arr);
-   alert(arr);
+   if (num.length > 0){
+      val.innerHTML = '';
+      arr.push(num);
+      console.log(arr);
+      
+   }else {
+      val.innerHTML = 'Please input an integer! :((';
+   }
+   
+}
+
+function sortedArray(){
+   var squaringArr = arr;
+   arr = [];
+   console.log(squaringArr);
+   squaringArr.forEach(num =>{
+     squaredNum = num ** 2;
+     arr.push(squaredNum);
+   });
+   for (let index = 0; index < arr.length; index++) {
+      arr.forEach((num, key) => { 
+         temp = num[index];
+         console.log(temp);
+         if (temp < num) {
+            sortedArr.push(num);
+         }
+         
+      });
+   }
+   
+   console.log(sortedArr);
 }
